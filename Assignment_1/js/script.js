e8224a3effbd7f7666d4e2f74a6f0fa6ea2c3cb0
Junior Francisco Garcia Ayala <junior.f.garcia99@gmail.com>
2020-02-15 15:16:55 +0400

    Added scroll code

--- Assignment_1/js/script.js
+++ Assignment_1/js/script.js
@@ -4,33 +4,52 @@ var open = "media/open.png";
 var close = "media/closed.png";
 var happy = "media/happy.jpg"
 var img_srcs = [
-    "media/D2.jpg",
+    "media/d2.png",
     "media/starbucks.png",
-    "media/mealclub.jpg",
-    "media/vegetarian.jpg",
-    "media/homebaked.jpg",
-    "media/thegrill.jpg",
+    "media/mealclub.png",
+    "media/vegetarian.png",
+    "media/homebaked.png",
+    "media/thegrill.png",
     "media/saj.png",
-    "media/feelgood.jpg",
-    "media/hotwok.jpg",
-    "media/breakfast.jpg",
-    "media/saladbar.jpg",
-    "media/vegan.jpg",
-    "media/deserts.jpg",
-    "media/grabandgo.jpg",
-    "media/soups.jpg",
+    "media/feelgood.png",
+    "media/hotwok.png",
+    "media/breakfast.png",
+    "media/saladbar.png",
+    "media/vegan.png",
+    "media/deserts.png",
+    "media/grabandgo.png",
+    "media/soups.png",
     "media/30mff.png"
 ]
+
+$(".D2img").click(function(){
+    console.log("D2 IMG CLICKED");
+})
 $(document).ready(function(){
+
+    $(".D2img").click(function(){
+        console.log("D2 IMG CLICKED");
+        // console.log($(this).attr('id'));
+        var id = $(this).attr('id');
+        var idnum= id.replace("img",'');
+        console.log(idnum)
+        $('html,body').animate({
+            scrollTop: $('#'+idnum).offset().top},
+            'slow');
+            
+
+    })
     console.log("Hiiiii")
     animate();
+    var d2img= $(".D2img");
+    console.log(d2img);
 })
 var requestAnimationFrame = window.requestAnimationFrame       ||
                             window.webkitRequestAnimationFrame ||
                             window.mozRequestAnimationFrame    ||
                             window.msRequestAnimationFrame; 
 var time = 0;
-var fps = 1; //frames per second to determine how many frames I want per second   
+var fps = 2; //frames per second to determine how many frames I want per second   
 
 const animate = () => {
     //use set timeout function to slowdown animation frame.
@@ -126,17 +145,11 @@ const draw = () => {
     else if (time == 17){
         console.log(time);
         change_image(11,img_srcs[14]);
-        change_image(15,open);   
-    }
-    else if (time == 18){
-        console.log(time);
-        change_image(15,happy);  
+        change_image(15,happy);    
     }
     else if (time == 19){
         console.log(time);
         change_image(15,img_srcs[15])
-    }
-    else{
         time = 0;
     }
 }
@@ -144,7 +157,6 @@ const draw = () => {
 function change_image(i,src) {
     
     var images = $(".headerimg");
-    console.log(images)
     $(images[i]).attr("src",src);
   }
 
