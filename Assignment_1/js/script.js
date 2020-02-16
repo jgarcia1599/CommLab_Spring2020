e2a396a77c5df6884526ff547335a3aae6619e85
Junior Francisco Garcia Ayala <junior.f.garcia99@gmail.com>
2020-02-16 17:23:42 +0400

    almost

--- Assignment_1/js/script.js
+++ Assignment_1/js/script.js
@@ -22,15 +22,27 @@ var img_srcs = [
     "media/30mff.png"
 ]
 
-$(".D2img").click(function(){
-    console.log("D2 IMG CLICKED");
-})
+var obj = document.createElement("audio");
+obj.src = "media/pacman2.wav";
+obj.volume = 0.4;
+obj.autoPlay = false;
+obj.preLoad = true;
+obj.controls = true;
+
+var yay = document.createElement("audio");
+yay.src = "media/yay.wav";
+yay.volume = 0.7;
+yay.autoPlay = false;
+yay.preLoad = true;
+yay.controls = true;
+
 $(document).ready(function(){
 
-    $(".D2img").click(function(){
+
+    $(".headerimg").click(function(){
         console.log("D2 IMG CLICKED");
-        // console.log($(this).attr('id'));
         var id = $(this).attr('id');
+        console.log(id);
         var idnum= id.replace("img",'');
         console.log(idnum)
         $('html,body').animate({
@@ -44,17 +56,19 @@ $(document).ready(function(){
             scrollTop: $('main').offset().top},
             'slow'); 
     })
-    console.log("Hiiiii")
+    $('#uparrow').click(function(){
+        $('html,body').animate({
+            scrollTop: $('header').offset().top},
+            'slow'); 
+    })
     animate();
-    var d2img= $(".D2img");
-    console.log(d2img);
 })
 var requestAnimationFrame = window.requestAnimationFrame       ||
                             window.webkitRequestAnimationFrame ||
                             window.mozRequestAnimationFrame    ||
                             window.msRequestAnimationFrame; 
 var time = 0;
-var fps = 2; //frames per second to determine how many frames I want per second   
+var fps = 3; //frames per second to determine how many frames I want per second   
 
 const animate = () => {
     //use set timeout function to slowdown animation frame.
@@ -79,6 +93,7 @@ const draw = () => {
         console.log(time); 
         change_image(0,img_srcs[0]); 
         change_image(4,open);
+        obj.play();
     }
     else if (time == 4){
         console.log(time);
@@ -88,7 +103,8 @@ const draw = () => {
     else if (time == 5){
         console.log(time);
         change_image(8,img_srcs[2]);
-        change_image(12,open); 
+        change_image(12,open);
+        obj.play(); 
     }
     else if (time == 6){
         console.log(time); 
@@ -98,7 +114,8 @@ const draw = () => {
     else if (time == 7){
         console.log(time);
         change_image(1,img_srcs[4]);
-        change_image(5,open);     
+        change_image(5,open);  
+        obj.play();   
     }
     else if (time == 8){
         console.log(time); 
@@ -109,6 +126,7 @@ const draw = () => {
         console.log(time);
         change_image(9,img_srcs[6]);
         change_image(13,open); 
+        obj.play();
     }
     else if (time == 10){
         console.log(time);
@@ -119,7 +137,8 @@ const draw = () => {
         console.log(time);
         //jdjdjkdjd
         change_image(2,img_srcs[8]);
-        change_image(6,open)   
+        change_image(6,open) 
+        obj.play();  
     }
     else if (time == 12){
         console.log(time);
@@ -129,7 +148,8 @@ const draw = () => {
     else if (time == 13){
         console.log(time);
         change_image(10,img_srcs[10]);
-        change_image(14,open);   
+        change_image(14,open);
+        obj.play();   
     }
     else if (time == 14){
         console.log(time);
@@ -140,7 +160,8 @@ const draw = () => {
         console.log(time);
         //jdjdjkdjd
         change_image(3,img_srcs[12]);
-        change_image(7,open)   
+        change_image(7,open);
+        obj.play();   
     }
     else if (time == 16){
         console.log(time);
@@ -150,9 +171,10 @@ const draw = () => {
     else if (time == 17){
         console.log(time);
         change_image(11,img_srcs[14]);
-        change_image(15,happy);    
+        change_image(15,happy);
+        yay.play();    
     }
-    else if (time == 19){
+    else if (time == 50){
         console.log(time);
         change_image(15,img_srcs[15])
         time = 0;
