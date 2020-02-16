37aad48d70c970c9fe76172c4d9808abb56cef09
Junior Francisco Garcia Ayala <junior.f.garcia99@gmail.com>
2020-02-16 23:03:35 +0400

    final code

--- Assignment_1/js/script.js
+++ Assignment_1/js/script.js
@@ -21,7 +21,8 @@ var img_srcs = [
     "media/soups.png",
     "media/30mff.png"
 ]
-
+//Audio elements
+var playanimation = true; //variable will be turned false whenever the user scrolls past the animation canvas
 var obj = document.createElement("audio");
 obj.src = "media/pacman2.wav";
 obj.volume = 0.4;
@@ -37,6 +38,8 @@ yay.preLoad = true;
 yay.controls = true;
 
 $(document).ready(function(){
+    const height = window.scrollY;
+    console.log(height);
     $(".headerimg").click(function(){
         console.log("D2 IMG CLICKED");
         var id = $(this).attr('id');
@@ -75,111 +78,123 @@ const animate = () => {
     
   }
 const draw = () => {
+    //Check if the user is within range for the audio to be played
+    if (window.scrollY > 1300){
+        playanimation = false;
+
+    }
+    else{
+        playanimation = true;
+    }
     time++;
     if (time == 1){
-        console.log(time);
         change_image(0,sad); 
     }
     else if (time == 2){
-        console.log(time);
         change_image(0,look); 
 
     }
     else if (time == 3){
-        console.log(time); 
         change_image(0,img_srcs[0]); 
         change_image(4,open);
-        obj.play();
+        if(playanimation == true)
+        {
+            obj.play();
+        }
+        
     }
     else if (time == 4){
-        console.log(time);
         change_image(4,img_srcs[1]);
         change_image(8,close);
     }
     else if (time == 5){
-        console.log(time);
         change_image(8,img_srcs[2]);
         change_image(12,open);
-        obj.play(); 
+        if(playanimation == true)
+        {
+            obj.play();
+        }
     }
     else if (time == 6){
-        console.log(time); 
         change_image(12,img_srcs[3]);
         change_image(1,close);  
     }
     else if (time == 7){
-        console.log(time);
         change_image(1,img_srcs[4]);
         change_image(5,open);  
-        obj.play();   
+        if(playanimation == true)
+        {
+            obj.play();
+        }  
     }
     else if (time == 8){
-        console.log(time); 
         change_image(5,img_srcs[5]);
         change_image(9,close);
     }
     else if (time == 9){
-        console.log(time);
         change_image(9,img_srcs[6]);
         change_image(13,open); 
-        obj.play();
+        if(playanimation == true)
+        {
+            obj.play();
+        }
     }
     else if (time == 10){
-        console.log(time);
         change_image(13,img_srcs[7]);
         change_image(2,close)   
     }
     else if (time == 11){
-        console.log(time);
-        //jdjdjkdjd
         change_image(2,img_srcs[8]);
         change_image(6,open) 
-        obj.play();  
+        if(playanimation == true)
+        {
+            obj.play();
+        } 
     }
     else if (time == 12){
-        console.log(time);
         change_image(6,img_srcs[9]);
         change_image(10,close)   
     }
     else if (time == 13){
-        console.log(time);
         change_image(10,img_srcs[10]);
         change_image(14,open);
-        obj.play();   
+        if(playanimation == true)
+        {
+            obj.play();
+        }   
     }
     else if (time == 14){
-        console.log(time);
         change_image(14,img_srcs[11]);  
         change_image(3,close);  
     }
     else if (time == 15){
-        console.log(time);
-        //jdjdjkdjd
         change_image(3,img_srcs[12]);
         change_image(7,open);
-        obj.play();   
+        if(playanimation == true)
+        {
+            obj.play();
+        }  
     }
     else if (time == 16){
-        console.log(time);
         change_image(7,img_srcs[13]);
         change_image(11,close)   
     }
     else if (time == 17){
-        console.log(time);
         change_image(11,img_srcs[14]);
         change_image(15,happy);
-        yay.play();    
+        if(playanimation == true)
+        {
+            yay.play();
+        }    
     }
     else if (time == 23){
-        console.log(time);
         change_image(15,img_srcs[15])
     }
-    else if (time == 45){
-        //End delay
-        console.log(time)
+    else if (time == 45){ //delay to easen transition between animation repetitions
         time=0
     }
 }
+
 function change_image(i,src) {
     
     var images = $(".headerimg");
