0e403575ea325b24d5b1324cdd7f45ca89980d00
Junior Francisco Garcia Ayala <junior.f.garcia99@gmail.com>
2020-02-17 09:09:55 +0400

    docs

--- Assignment_1/js/script.js
+++ Assignment_1/js/script.js
@@ -78,7 +78,6 @@ const animate = () => {
     
   }
 const draw = () => {
-    //Check if the user is within range for the audio to be played
     if (window.scrollY > 1300){
         playanimation = false;
 
@@ -88,13 +87,16 @@ const draw = () => {
     }
     time++;
     if (time == 1){
+        console.log(time);
         change_image(0,sad); 
     }
     else if (time == 2){
+        console.log(time);
         change_image(0,look); 
 
     }
     else if (time == 3){
+        console.log(time); 
         change_image(0,img_srcs[0]); 
         change_image(4,open);
         if(playanimation == true)
@@ -104,10 +106,12 @@ const draw = () => {
         
     }
     else if (time == 4){
+        console.log(time);
         change_image(4,img_srcs[1]);
         change_image(8,close);
     }
     else if (time == 5){
+        console.log(time);
         change_image(8,img_srcs[2]);
         change_image(12,open);
         if(playanimation == true)
@@ -116,10 +120,12 @@ const draw = () => {
         }
     }
     else if (time == 6){
+        console.log(time); 
         change_image(12,img_srcs[3]);
         change_image(1,close);  
     }
     else if (time == 7){
+        console.log(time);
         change_image(1,img_srcs[4]);
         change_image(5,open);  
         if(playanimation == true)
@@ -128,10 +134,12 @@ const draw = () => {
         }  
     }
     else if (time == 8){
+        console.log(time); 
         change_image(5,img_srcs[5]);
         change_image(9,close);
     }
     else if (time == 9){
+        console.log(time);
         change_image(9,img_srcs[6]);
         change_image(13,open); 
         if(playanimation == true)
@@ -140,10 +148,13 @@ const draw = () => {
         }
     }
     else if (time == 10){
+        console.log(time);
         change_image(13,img_srcs[7]);
         change_image(2,close)   
     }
     else if (time == 11){
+        console.log(time);
+        //jdjdjkdjd
         change_image(2,img_srcs[8]);
         change_image(6,open) 
         if(playanimation == true)
@@ -152,10 +163,12 @@ const draw = () => {
         } 
     }
     else if (time == 12){
+        console.log(time);
         change_image(6,img_srcs[9]);
         change_image(10,close)   
     }
     else if (time == 13){
+        console.log(time);
         change_image(10,img_srcs[10]);
         change_image(14,open);
         if(playanimation == true)
@@ -164,10 +177,13 @@ const draw = () => {
         }   
     }
     else if (time == 14){
+        console.log(time);
         change_image(14,img_srcs[11]);  
         change_image(3,close);  
     }
     else if (time == 15){
+        console.log(time);
+        //jdjdjkdjd
         change_image(3,img_srcs[12]);
         change_image(7,open);
         if(playanimation == true)
@@ -176,10 +192,12 @@ const draw = () => {
         }  
     }
     else if (time == 16){
+        console.log(time);
         change_image(7,img_srcs[13]);
         change_image(11,close)   
     }
     else if (time == 17){
+        console.log(time);
         change_image(11,img_srcs[14]);
         change_image(15,happy);
         if(playanimation == true)
@@ -188,13 +206,15 @@ const draw = () => {
         }    
     }
     else if (time == 23){
+        console.log(time);
         change_image(15,img_srcs[15])
     }
-    else if (time == 45){ //delay to easen transition between animation repetitions
+    else if (time == 45){
+        //End delay
+        console.log(time)
         time=0
     }
 }
-
 function change_image(i,src) {
     
     var images = $(".headerimg");
