4e40d0c917b4b832ab33f23754a35278e9278d2d
Junior Francisco Garcia Ayala <junior.f.garcia99@gmail.com>
2020-03-15 14:02:46 +0400

    assignment2 update

--- Assignment_2/script.js
+++ Assignment_2/script.js
@@ -1,21 +1,43 @@
-var form_responses = []
 
-$('.text-block').hide()
-$('#input_form').on("submit",function(e) {
+
+$(document).ready(function(){
+    AOS.init();
+    const height = window.scrollY;
+    console.log(height);
+//To hide uncomment below: 
+// $('.scroll-section-main').hide();
+$('.scroll-section').hide();
+$('#welcome_message').hide()
+    var form_responses = []
+$('#input_form').submit(function(e) {
+
     e.preventDefault(); // cancel the actual submit
-
+    console.log("u submitted")
     var form= $(this).serialize();
     var form_elements = form.split("&");
     for(var i = 0; i<form_elements.length;i++){
         var answer = form_elements[i].split("=");
-        console.log(answer[1]);
+        // console.log(answer[1]);
         var final_answer = answer[1].replace(/%20/g," ");
         console.log(final_answer);
         form_responses.push(final_answer);
     }
-    console.log($('.text-block p').text)
-    $('.text-block p').text(form_responses[0]);
-    $('.text-block').show()
+    console.log(form_responses)
+    $('#input_form').hide();
+    $('#welcome_message').fadeIn("slow")
+    $('.scroll-section').show();
+    $('#image_text_1').text(form_responses[1]);
+    $('#image_text_2').text(form_responses[2]);
+    $('#image_text_3').text(form_responses[3]);
+    $('#image_text_4').text(form_responses[0]);
+    $('#image_text_5').text(form_responses[2]);
+
   });
+
+
+
+
+})
+
 
   
---
+++ examples/07_Comic Project Refs/6_Parallax Examples/js/script.js
@@ -0,0 +1,21 @@
+// window.addEventListener('scroll', () => {
+//   let parent =  document.getElementById('parallax-container');
+//   let children = parent.getElementsByTagName('div');
+//   for(let i = 0; i < children.length; i++) {
+//     children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
+//   }
+// }, false)
+
+
+window.addEventListener('scroll', doPar);
+
+function doPar()
+{
+  let parent =  document.getElementById('parallax-container');
+  let children = parent.getElementsByTagName('div');
+  for(let i = 0; i < children.length; i++) {
+    children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
+    // console.log(window.pageYOffset * i / children.length + 'px');
+    console.log(window.pageYOffset);
+  }
+}
